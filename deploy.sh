d48233eaac1e0c84adb5cfc0daa3ac3654dd44ff
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-11-22 04:53:27 -0400

    another attempt at getting the bash script right for CI

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-apps="$(./node_modules/.bin/nx affected:apps --plain)"
+echo "Getting affected apps"
+echo "Running ./node_modules/.bin/nx affected:apps --base=remotes/origin/main --plain"
+apps="$(./node_modules/.bin/nx affected:apps --base=remotes/origin/main --plain)"
 echo "$apps"
 
+echo "Starting for loop"
 for targetName in $apps
 do
+    echo "Printing target name"
     echo "$targetName"
+
     if [ -z $1 ]
     then
+        echo "Local build"
         firebase deploy --only hosting:$targetName
     else
+        echo "Production build"
         firebase deploy --token $1 --only hosting:$targetName
     fi
     # 

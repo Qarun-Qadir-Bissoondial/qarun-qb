ce79b22bc964c123605494e6f4102077eac2371b
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-11-29 00:51:44 -0400

    Changed some spacing. Added meta theme-color and description

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'qarun-qb-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QarunQB';
+  largeScreen$: Observable<boolean>;
+
+  constructor(private breakpoint: BreakpointObserver) {
+    this.largeScreen$ = this.breakpoint.observe('(min-width: 768px)').pipe(map(bp => bp.matches));
+  }
+
+  scroll(selector: string) {
+    const { top, left } = document.querySelector(selector).getBoundingClientRect();
+    window.scroll({
+      top,
+      left,
+      behavior: 'smooth'
+    });
+  }
 }
